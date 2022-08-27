8933672c1a8ba4b3c01b1b8eb37843942c57f2ce
BuildTools <unconfigured@null.spigotmc.org>
2022-08-27 08:41:07 -0500

    fixing ttt mentions, cleanup, /restart and /shutdown

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 hey everyone! this is the code for the proto foxy discord bot made by carter and moon puppy prototype
 
 ## how to run this
-it's pretty easy, just run
+make sure you have python3.9 and pip3.9 installed and then run:
 ```
 git clone https://github.com/MrUbun2/proto-foxy-bot
 cd proto-foxy-bot
-python3 -m pip install requirements.txt
+python3.9 -m pip install requirements.txt
 export TOKEN = [your discord bot token here]
-python3 ./protofoxy/__main__.py
+chmod +x ./protofoxy/__main__.py
+./protofoxy/__main__.py
 ```
 and it's up!
--- protofoxy/__main__.py
+++ protofoxy/__main__.py
@@ -1,7 +1,10 @@
+#! /usr/bin/env python3.9
 from discord.ext import commands
 from discord import Option
-import discord, os
+import discord, asyncio, sys, os
 
+TIMES = [30,20,15,10,3,2,1,0]
+RESTART = False
 bot = discord.Bot(auto_sync_commands=True)
 
 class TTT(discord.ui.View):
@@ -9,17 +12,13 @@ class TTT(discord.ui.View):
         super().__init__(timeout=60)
         self.t=False
         self.p=p
-    # idk why this isn't working:
-    # async def on_timeout(self):
-    #     for child in self.children:
-    #         child.disabled = True
-    #         print(self.message)
-    #     await self.message.edit(content="You took too long!", view=self)
+    async def on_timeout(self):
+        for child in self.children:
+            child.disabled = True
     async def check(self):
         c=[]
         for child in self.children:
             c.append(child.label)
-        print(c)
         g=[
             [1,1,1,0,0,0,0,0,0],
             [0,0,0,1,1,1,0,0,0],
@@ -39,13 +38,10 @@ class TTT(discord.ui.View):
                         t+=1
                         if c[i]==l:
                             b+=1
-                print(t,b)
                 if t==b:
                     return l
         return False
     async def m(self, button, interaction):
-        print(type(interaction.user))
-        print(type(self.p[self.t]))
         if interaction.user == self.p[self.t]:
             self.t=not self.t
             button.disabled=True
@@ -53,14 +49,13 @@ class TTT(discord.ui.View):
             button.label=["X","O"][self.t]
             b=await self.check()
             if b:
-                print(b)
                 for child in self.children:
                     child.disabled=True
                 if b=="X":
                     w=self.p[1]
                 else:
                     w=self.p[0]
-                await interaction.response.edit_message(content=f"{w} won!", view=self)
+                await interaction.response.edit_message(content=f"<@{w.id}> won!", view=self)
             else:
                 await interaction.response.edit_message(view=self)
         else:
@@ -87,6 +82,10 @@ class TTT(discord.ui.View):
     @discord.ui.button(label="\u200b", row=2, style=discord.ButtonStyle.gray)
     async def i(self, button, interaction): await self.m(button, interaction)
 
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
 @bot.command(description="Help!")
 async def help(ctx):
     await ctx.respond(f"Hi there, this is a simple bot that has (currently) a few moderation commands and some other fun stuff.\nCommands:\n/help: shows this page\n/ping: shows the bot\'s latency\n/kick: kicks a user\n/ban: bans a user", ephemeral=True)
@@ -95,10 +94,10 @@ async def help(ctx):
 async def ping(ctx):
     await ctx.respond(f"Pong! Latency is {bot.latency}")
 
-@bot.command(description="Simple tic tac toe game")
+@bot.user_command(name="Tic Tac Toe")
+@bot.command(description="Simple Tic Tac Toe game")
 async def tictactoe(ctx, member: Option(discord.Member, "The member you want to play against")):
-    print(member)
-    await ctx.respond(f"@{ctx.author} vs @{member} tic tac toe", view=TTT([ctx.author,member]))
+    await ctx.respond(f"<@{ctx.author.id}> vs <@{member.id}> tic tac toe", view=TTT([ctx.author,member]))
 
 @bot.command(description="Kicks a member")
 @commands.has_permissions(kick_members=True)
@@ -120,4 +119,35 @@ async def ban(ctx, member: Option(discord.Member, "The member you want to ban"),
     else:
         await ctx.respond(r)
 
-bot.run(os.getenv("TOKEN"))
+@bot.command(description="Shuts down the bot")
+@commands.is_owner()
+async def shutdown(ctx):
+    await ctx.respond("bot is shutting down aahhhhh")
+    print("--- SHUTDOWN INITIATED ---")
+    for k, i in enumerate(TIMES):
+        await bot.change_presence(status=discord.Status.dnd,activity=discord.Activity(type=3, name=f"shutting down in {i}"))
+        print(i)
+        if i!=0: await asyncio.sleep(TIMES[k]-TIMES[k+1])
+    print("--- SHUTDOWN COMPLETE ---")
+    await bot.close()
+
+@bot.command(description="Restarts down the bot")
+@commands.is_owner()
+async def restart(ctx):
+    global RESTART
+    await ctx.respond("bot is restarting aahhhhh")
+    RESTART = True
+    print("--- RESTART INITIATED ---")
+    for k, i in enumerate(TIMES):
+        await bot.change_presence(status=discord.Status.dnd,activity=discord.Activity(type=3, name=f"restarting in {i}"))
+        print(i)
+        if i!=0: await asyncio.sleep(TIMES[k]-TIMES[k+1])
+    print("--- RESTART COMPLETE ---")
+    await bot.close()
+
+if __name__ == "__main__":
+    bot.run(os.getenv("TOKEN"))
+    print(RESTART)
+    if RESTART:
+        print('resrar')
+        os.execv(__file__, sys.argv)
